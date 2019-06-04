5dbefa59772812882be2210e60f079cb6d2a4f6b
Souvik Kundu <kivtech88@gmail.com>
2019-06-04 20:11:43 +0530

    Initial commit

---
+++ arrays.js
@@ -0,0 +1,5 @@
+/*
+Arrays:» Write JavaScript to display the  names of computer languages and display all of them in an alert box on click of a button.  Hint: Use arrays to store the list ofcomputer programming languages
+*/
+
+var computerLanguages = ["JavaScript", "Python", "Java"]
